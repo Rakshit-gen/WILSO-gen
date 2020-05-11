6406d0cc9f85db069b39c9e2ae215205a55c6bf7
Rakshit-gen <64471316+Rakshit-gen@users.noreply.github.com>
2020-05-11 18:40:19 +0530

    Update hindi_writer.py

--- hindi_writer.py
+++ hindi_writer.py
@@ -4,8 +4,8 @@ with sr.Microphone() as source:
     while 1==1:
         audio=r.listen(source)
         try:
-            query=r.recognize_google(audio,language='hi-In')
-            print('YOU SAID',query)
+            query=r.recognize_google(audio,language='hi-In')     #Say hindi words as clear as possible
+            print('YOU SAID',query)                              #Here you will recive what you have said
         except Exception as e:
-            print('say that again')
+            print('say that again')                              #Just to cope up with exceptions
             pass
